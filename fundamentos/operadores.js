db9f3c42f72852e7e0d04e4a5c7cab701a3d22f2
Murillo Mendonca Mascarenha <mmendoncamascarenha@gmail.com>
2025-01-22 17:17:16 -0300

    base de operacoes no JS

---
+++ fundamentos/operadores.js
@@ -0,0 +1,38 @@
+/**
+ * OPERADORES
+ * -Aritméticos
+ * -Aritiméticos de atribuição
+ * comparação
+ * lógicos
+ */
+
+console.clear()
+console.log("Operadores aritmétricos")
+console.log("+ | soma")
+console.log("- | sutração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo(rest da divisão)")
+console.log("")
+console.log("Operadores aritmétricos de atribuição")
+console.log("+= | soma e atribuição varíavel")
+console.log("-= | subtração e atribuição variavel")
+console.log("*= | multiplicação e atribuição a variavel")
+console.log("/=| divisão e atribuição variavel")
+console.log("++ | soma 1 ao resultado")
+console.log("-- | subtração 1 do resultado")
+console.log("")
+console.log("Comparadores")
+console.log("> | maior que")
+console.log("< | menor que")
+console.log(">= | maior ou igual a ")
+console.log("<= | maior ou igual a")
+console.log("=== | identico a")
+console.log("!== | não idêntico a")
+console.log("")
+console.log("Operadores lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("! | NOT")
+
+
